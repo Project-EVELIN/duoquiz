a4b892b70be4b2f75e62bdb1777482161f4a1215
Michael Ebert <michael.ebert@gmail.com>
2014-08-21 09:10:14 +0200

    :fire: Fixes char, string prefixes for lexing

--- compiler/cpp/parser_test.js
+++ compiler/cpp/parser_test.js
@@ -1,13 +1,14 @@
 document.body.onload = function(){
-	var DEBUG_LEXING = false;
-	var input_decl = "int i = 4;";
+	var DEBUG_CPP = false;
+	var DEBUG_EBNF = true;
+	var input_decl = "char c = u's';";
 	var input_prepmain = "#include <stdio.h>\nint main(void){}";
 	var input_main = "int main(void){}";
 	var input_prep = "#include <stdio.h>\n#define MAX 32\n";
 	var input = input_decl;
 
 	// initialize lexer
-	if(DEBUG_LEXING) {
+	if(DEBUG_CPP) {
 	var lexer = cpp.lexer.setInput(input);
 		lexer.yy = cpp.yy;
 		lexer.yy.lexer = lexer;
@@ -29,6 +30,26 @@ document.body.onload = function(){
 		console.log("\n\nParsing cpp.js result: ", parse_result);
 	} catch(e) {
 		console.log(e);
+	}
+
+	console.log("\n**** ENBF TEST ***\n");
+
+	/**** ENBF TEST ***/
+	// initialize lexer
+	if(DEBUG_EBNF) {
+	var lexer = ebnf.lexer.setInput(input);
+		lexer.yy = ebnf.yy;
+		lexer.yy.lexer = lexer;
+		lexer.yy.parser = ebnf;
+		if (typeof lexer.yylloc == 'undefined')
+			lexer.yylloc = {};
+
+		// tokenize and show rules and positions
+		var rule;
+		do {
+			rule = lexer.lex();
+			console.log(rule, ebnf.terminals_[rule], "\n" + lexer.showPosition(), "\n");
+		}while(rule != 1);
 	}
 
 	// try parsing
