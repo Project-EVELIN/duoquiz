c842b9f9c232e906deddeaedd0dd03a672531bf6
Michael Ebert <michael.ebert@gmail.com>
2014-08-22 09:43:54 +0200

    Adds simple flat array tokenzier for testing

--- compiler/cpp/parser_test.js
+++ compiler/cpp/parser_test.js
@@ -8,7 +8,7 @@ document.body.onload = function() {
 		"extern const int c = 1;",
 		"int f(int x) { return x+a; }",
 		"struct S { int a; int b; }; ",
-		"struct X {\nint x;\nstatic int y;\nX(): x(0) { }\};",
+		"struct X {int x;static int y;X(): x(0) { }\};",
 		"int X::y = 1;",
 		"enum { up, down };",
 		"namespace N { int d; }",
@@ -33,7 +33,50 @@ document.body.onload = function() {
 	var input_prep = "#include <stdio.h>\n#define MAX 32\n";
 	var input_empty_declaration = ";";
 	var input_extern_linkage_specification = "extern \"string-literal\" {}";
-	var input = input_declarations;
+	var input = input_definitions;
+
+	/* 	Tokenizes the given input and pushes each token in an array
+	 * 	@argument grammar:	jison generated javascript, must include lexer
+	 *		@argument input: 		string
+	 */
+	function lexToFlatArray(grammar, input) {
+		var arr = [];
+
+		var lexer = grammar.lexer.setInput(input);
+		lexer.yy = grammar.yy;
+		lexer.yy.lexer = lexer;
+		lexer.yy.parser = grammar;
+		if (typeof lexer.yylloc == 'undefined')
+			lexer.yylloc = {};
+
+		// tokenize and show rules and positions
+		var rule;
+		do {
+			rule = lexer.lex();
+			//console.log(rule, grammar.terminals_[rule], "\n" + lexer.showPosition(), "\n");
+			if (grammar.terminals_[rule] !== undefined) {
+				arr.push(grammar.terminals_[rule]);
+			} else if (rule === 1) {
+				// do nothing, EOF
+			} else if (rule === "IDENTIFIER" || rule === "DECIMAL_LITERAL" || rule === "OCTAL_LITERAL" || rule === "HEXADECIMAL_LITERAL" || rule === "FLOATING_LITERAL" || rule === "CHARACTER_LITERAL" || rule === "STRING_LITERAL") {
+				arr.push([rule, lexer.yytext]);
+			} else {
+				arr.push(rule);
+			}
+		} while (rule != lexer.EOF);
+
+		return arr;
+	}
+
+	// this is only lexing and only with enhanced ebnf lexer
+	if (Array.isArray(input)) {
+		var i;
+		for (i = 0; i < input.length; i++) {
+			console.log(lexToFlatArray(ebnf, input[i]));
+		}
+	} else {
+		console.log(lexToFlatArray(ebnf, input));
+	}
 
 	if (!Array.isArray(input)) {
 		// initialize lexer
