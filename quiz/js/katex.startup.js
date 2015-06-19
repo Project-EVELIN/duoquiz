81a8109e2d8a18e0e99c39ca74b02a9ba2fcbd2d
Michael Ebert <michael.ebert@gmail.com>
2015-06-19 19:58:40 +0200

    Cleanup

--- quiz/js/katex.startup.js
+++
@@ -1,9 +0,0 @@
-/*
- * render KateX snippets
- */
-document.addEventListener("DOMContentLoaded", function(event) {
-	var tex = document.getElementsByClassName("tex");
-	Array.prototype.forEach.call(tex, function(el) {
-	    katex.render(el.getAttribute("data-expr"), el);
-	});
-});
